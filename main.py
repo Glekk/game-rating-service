760f1346b49fcb8f77e7254b01b433e2bd30b784
Vitalii Shvets <91060482+Glekk@users.noreply.github.com>
2022-06-27 16:40:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     connection = mysql.connector.connect(
         host="localhost",
         user="someuser",
-        password="p4ssword",
-        database="game_service"
+        password="password",
+        database="game service"
     )
     interface.main_menu_guest(connection)
 
